c0ba9993e39fbe7e46f9166196c3850a3314c4aa
Vladi <example@gmail.com>
2021-12-21 12:06:03 +0900

    delte

--- js/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// alert("Hello Vladi");
-// const answer = +prompt("Are u drunk?", "No");
-const answers =[]
-answers[0] = prompt("Hor are u today'");
-answers[1] = prompt("Hor are u tmrw'");
-answers[2] = prompt("Hor are u next week");
-console.log(answers);
