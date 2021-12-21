3fd6215a7c5816c8b4b8a16029dde7efb946a17f
Vladi <example@gmail.com>
2021-12-22 00:24:43 +0900

    условия

---
+++ js/Условия.js
@@ -0,0 +1,39 @@
+if (4==4) {
+    console.log('OK!');
+} else  {
+    console.log('Error'); 
+   
+}
+
+const num = 50;
+
+// if (num<49) {
+//     console.log('Error');
+// } else if (num>100) {
+//     console.log ('Error');
+// } else {
+//     console.log("OK");
+// }
+
+num == 50 ? console.log("OK"): console.log('Error');
+
+
+const num = 49;
+
+switch (num){
+    case 49:
+        console.log('more');
+        break;
+    case 100:
+        console.log('down');
+        break;
+    case 50:
+        console.log('right');
+        break;
+    default:
+        console.log('not in this time');
+}
+
+
+
+
